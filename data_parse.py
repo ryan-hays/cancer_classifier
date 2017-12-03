c5354dfc05cea6ae0d6c0b10205ca6eecb0045c5
Ryan Hays <ryanhays@mit.edu>
2017-12-03 16:46:31 -0500

    Documentation added

--- data_parse.py
+++ data_parse.py
@@ -38,19 +38,25 @@ def get_genes_for_cancer_type(files, k=10):
     return gene_list
 
 
-def extract_data_files():
+def get_cancer_data_files(mutation_data_name):
     """
-    Returns a dictionary mapping cancer type to a list of data_mutation_files
+    Gets a dictionary mapping cancer type to a list of data files
+
+    Parameters: 
+    mutation_data_name: (str) the file name for the mutation data. Consistent across all
+    cancer types
+
+    Returns:
+    cancer_types_dict: (dict) cancer : list of data files
     """
-    name = "data_mutations_extended.txt"
     cancer_types_dict = {} #key = cancer_type, value = mutation data files
     cancer_types_folder = os.listdir("../cancer_classifier_data")
     #Iterate through every cancer type
     for cancer_type in cancer_types_folder:
         cancer_types_dict[cancer_type] = []
         for root, dirs, files in os.walk(os.path.join("../cancer_classifier_data", cancer_type)):
-            if name in files:
-                cancer_types_dict[cancer_type].append(os.path.join(root, name)) #collect all files of data for each cancer type
+            if mutation_data_name in files:
+                cancer_types_dict[cancer_type].append(os.path.join(root, mutation_data_name)) #collect all files of data for each cancer type
     return cancer_types_dict
 
 def readdata(file):
@@ -63,27 +69,57 @@ def readdata(file):
     return data
 
 def gene_features():
-    cancer_type_dict = extract_data_files()
-    gene_features = []
+    """
+    Select genes to be used as features
+
+    Returns:
+    gene_set: (list) contains HUGO names of the genes being used as the features 
+    """
+    cancer_type_dict = get_cancer_data_files(mutation_data_name="data_mutations_extended.txt")
+    gene_set = []
     #Pull genes for each cancer type
     for cancer in cancer_type_dict:
         cancer_files = cancer_type_dict[cancer]
         genes = get_genes_for_cancer_type(cancer_files)
         for gene in genes:
-            if gene not in gene_features: #make sure gene hasn't already been selected in another type
-                gene_features.append(gene)
-    return gene_features, cancer_type_dict
+            if gene not in gene_set: #make sure gene hasn't already been selected in another type
+                gene_set.append(gene)
+    return gene_set
 
-def feature_build(gene_set, file_paths_by_cancer):
-    #Build list of samples
-    cancer_dict = {} #maps cancer to an index
+def get_cancer_types(file_paths_by_cancer):
+    """
+    Maps cancer type to an index value 
+
+    Parameters:
+    file_paths_by_cancer: (dict) cancer : list of data files 
+
+    Returns:
+    cancer_dict: (dict) cancer : integer identifier
+    """
+    cancer_dict = {}
     cancer_index = 0
+    for cancer in file_paths_by_cancer:
+        cancer_dict[cancer] = cancer_index
+        cancer_index += 1
+    return cancer_dict
+
+def get_samples(file_paths_by_cancer, cancer_dict, feature_vector_length):
+    """
+    Intializes a feature vector for samples in dataset and maps samples to their cancer type 
+
+    Parameters:
+    file_paths_by_cancer: (dict) cancer : list of data files 
+    cancer_dict: (dict) cancer : integer identifier
+    feature_vector_length: (int) length of feature vector
+
+    Returns:
+    training_set: (dict) sample : initialized feature vector 
+    training_set_targets: (dict) sample : cancer integer identifer
+    """
     training_set = {}
     training_set_targets = {}
     #Intialize training set and training set targets with all zeros
     for cancer in file_paths_by_cancer:
-        cancer_dict[cancer] = cancer_index
-        cancer_index += 1
         list_of_files = file_paths_by_cancer[cancer]
         for f in list_of_files:
             try:
@@ -92,13 +128,26 @@ def feature_build(gene_set, file_paths_by_cancer):
                 for i in data[1:]:
                     if i[t_barcode_index] not in training_set: # and i[t_barcode_index] != 'Tumor_Sample_Barcode':
                         sample_id = i[t_barcode_index]
-                        training_set[sample_id] = [0 for i in range(len(gene_set))]
+                        training_set[sample_id] = [0 for i in range(feature_vector_length)]
                         training_set_targets[sample_id] = cancer_dict[cancer]
             except Exception as e:
                 print(e)
                 pass
-    #print("Here", training_set)
-    #Update when finding genes
+    return training_set, training_set_targets
+
+def get_genes_for_samples_binary(gene_set, training_set, file_paths_by_cancer):
+    """
+    Builds a binary feature vector for each sample in the data set. Gene is "1" if mutated in
+    given sample; "0" else
+
+    Parameters:
+    gene_set: (list) contains HUGO names of the genes being used as the features 
+    training_set: (dict) sample : feature vector, where "feature vector" contains mutation data
+    file_paths_by_cancer: (dict) cancer : list of data files 
+
+    Returns:
+    training_set: mutated "training_set" parameters where genes are set to "1" if mutated in sample
+    """
     for cancer in file_paths_by_cancer:
         list_of_files = file_paths_by_cancer[cancer]
         for f in list_of_files:
@@ -111,25 +160,42 @@ def feature_build(gene_set, file_paths_by_cancer):
                     if gene in gene_set:
                         training_set[sample_id][gene_set.index(gene)] = 1
             except:
+                #TODO fix error handling
                 pass
-    #Concert to array 
-    #print(len(training_set))
-    print training_set
-    print training_set_targets
+    return training_set
+
+def save_data_set(training_set, training_set_targets, dataset_name):
+    """
+    Saves training set feature vectors and target values as numpy array
+
+    Parameters:
+    training_set: (dict) sample : feature vector, where "feature vector" contains mutation data
+    training_set_targets: (dict) sample : cancer integer identifer
+    dataset_name: (str) dataset name for saving numpy array
+    """
     sample_list = training_set.keys()
     training_set_final = [training_set[val] for val in sample_list]
     training_set_targets_final = [training_set_targets[val] for val in sample_list]
-    np.save('v2_training_set.npy', np.array(training_set_final))
-    np.save('v2_training_set_targets.npy', np.array(training_set_targets_final))
-    #print(gene_set)
+    np.save(dataset_name+".npy", np.array(training_set_final))
+    np.save(dataset_name+"_targets"+".npy", np.array(training_set_targets_final))
 
+def feature_build(gene_set):
+    file_paths_by_cancer = get_cancer_data_files(mutation_data_name="data_mutations_extended.txt")
+    cancers = get_cancer_types(file_paths_by_cancer) #Find cancers
+    samples, training_set_targets = get_samples(file_paths_by_cancer, 
+                                                cancer_dict=cancers, 
+                                                feature_vector_length=len(gene_set)) #Build list of samples    
+    training_set = get_genes_for_samples_binary(gene_set, samples, file_paths_by_cancer) #Update feature vectors 
+    print(training_set)
+    #Save targets
+    save_data_set(training_set, training_set_targets, dataset_name="TEST")
 
-
-
+def main():
+    genes = gene_features() #select genes that we will use as gene features 
+    feature_build(gene_set=genes)#Now build feature vector for each sample in study 
 
 if __name__ == "__main__":
-    genes, cancer_types_dict = gene_features() #select genes that we will use as gene features 
-    feature_build(gene_set=genes, file_paths_by_cancer=cancer_types_dict)#Now build feature vector for each sample in study 
+    main()
 
 
 
